cac6312af8a64e0e2679e4fdf9e334a3927e462b
sukumar <sukumarg.7310@gmail.com>
2023-12-20 19:42:04 +0530

    catalogue roboshop

---
+++ catalogue.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/temp/$0.$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+MONGODB_HOST=mongo.aarchdevops.online
+
+echo "This script is started executed at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$B ERROR $N...$R Please run this script with Root user $N"
+    exit 1
+else
+    echo -e "$Y This is Root user $N"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling NodeJS"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "Enabling NodeJS"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing NodeJS"
+
+useradd roboshop &>> $LOGFILE
+VALIDATE $? "Adding roboshop user"
+
+mkdir /app &>> $LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Downloading Catalogue application"
+
+cd /app
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Unzipping catalogue"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "Copying catalogue service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Catalogue daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enabling Catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB Client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE $? "Loading catalogue data into MongoDB"
